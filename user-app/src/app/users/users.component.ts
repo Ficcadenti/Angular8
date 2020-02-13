f08925822ca984fb6a1e28e236ae8e869270ad9c
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2020-02-13 13:30:33 +0100

    Aggiunta componente

--- user-app/src/app/users/users.component.ts
+++ user-app/src/app/users/users.component.ts
@@ -10,5 +10,34 @@ import { Component } from '@angular/core';
 )
 
 export class UsersComponent {
-
+    title: string = 'Users';
+    users: Array<{}> = [
+        {
+            nome: 'Raffaele',
+            cognome: 'Ficcadenti',
+            cf: 'XXXXXX',
+            mail: 'raffaele.ficcadenti@gmail.com',
+            telefono: 'XXX-XXXXXXX',
+            provincia: 'Roma',
+            eta: 43
+        },
+        {
+            nome: 'Raffaele1',
+            cognome: 'Ficcadenti',
+            cf: 'XXXXXX',
+            mail: 'raffaele.ficcadenti@gmail.com',
+            telefono: 'XXX-XXXXXXX',
+            provincia: 'Roma',
+            eta: 43
+        },
+        {
+            nome: 'Raffaele2',
+            cognome: 'Ficcadenti',
+            cf: 'XXXXXX',
+            mail: 'raffaele.ficcadenti@gmail.com',
+            telefono: 'XXX-XXXXXXX',
+            provincia: 'Roma',
+            eta: 43
+        }
+    ];
 }
